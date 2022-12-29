b6a2b4b35aec6862982bffb0c2b072e9ae6d68ec
k-phi <kraft.philipp@gmail.com>
2022-12-29 18:11:32 +0100

    Do exercise 2

--- src/test/java/caffeinateme/steps/OrderCoffeeSteps.java
+++ src/test/java/caffeinateme/steps/OrderCoffeeSteps.java
@@ -13,25 +13,46 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class OrderCoffeeSteps {
-    Customer cathy = Customer.named("Cathy");
+    Customer customer;
     CoffeeShop coffeeShop = new CoffeeShop();
     Order order;
+
+    @Given("{} is a CaffeinateMe customer")
+    public void a_caffeinate_me_customer_named(String customerName) {
+        customer = coffeeShop.registerNewCustomer(customerName);
+    }
+
     @Given("Cathy is {int} metres from the coffee shop")
     public void cathy_is_metres_from_the_coffee_shop(Integer distanceInMeters) {
-        cathy.setDistanceFromShop(distanceInMeters);
+        customer.setDistanceFromShop(distanceInMeters);
     }
-    @When("Cathy orders a {}")
+
+    @When("Cathy orders a {string}")
     public void cathy_orders_a(String orderedProduct) {
-        this.order = Order.of(1, orderedProduct).forCustomer(cathy);
-        cathy.placesAnOrderFor(order).at(coffeeShop);
+        this.order = Order.of(1, orderedProduct).forCustomer(customer);
+        customer.placesAnOrderFor(order).at(coffeeShop);
     }
+
+    @When("Cathy orders a {string} with a comment {string}")
+    public void cathy_orders_a_with_a_comment(String orderedProduct, String comment) {
+        this.order = Order.of(1, orderedProduct).forCustomer(customer).withComment(comment);
+        customer.placesAnOrderFor(order).at(coffeeShop);
+    }
+
     @Then("Barry should receive the order")
     public void barry_should_receive_the_order() {
         assertThat(coffeeShop.getPendingOrders(), hasItem(order));
     }
+
     @Then("Barry should know that the order is {}")
     public void barry_should_know_that_the_order_is(OrderStatus expectedStatus) {
-        Order cathysOrder = coffeeShop.getOrderFor(cathy).orElseThrow(() -> new AssertionError("No order found!"));
-        assertThat(cathysOrder.getStatus(), is(expectedStatus));
+        Order customersOrder = coffeeShop.getOrderFor(customer).orElseThrow(() -> new AssertionError("No order found!"));
+        assertThat(customersOrder.getStatus(), is(expectedStatus));
+    }
+
+    @Then("the order should have the comment {string}")
+    public void the_order_should_have_the_comment(String comment) {
+        Order customersOrder = coffeeShop.getOrderFor(customer).orElseThrow(() -> new AssertionError("No order found!"));
+        assertThat(customersOrder.getComment(), is(comment));
     }
 }
--- src/test/resources/features/orders/order_a_coffee.feature
+++ src/test/resources/features/orders/order_a_coffee.feature
@@ -5,8 +5,9 @@ Feature: Order a coffee
   I want to be able to order my coffee in advance
 
   Scenario Outline: Buyer orders a coffee when they are close to the coffee shop
-    Given Cathy is <distance> metres from the coffee shop
-    When Cathy orders a large cappuccino
+    Given Cathy is a CaffeinateMe customer
+    And Cathy is <distance> metres from the coffee shop
+    When Cathy orders a "large cappuccino"
     Then Barry should receive the order
     And Barry should know that the order is <expected status>
 
@@ -15,3 +16,9 @@ Feature: Order a coffee
       |       50 |          Urgent |
       |      100 |          Urgent |
       |      300 |          Normal |
+
+  Scenario: Buyers can add a comment with their order
+    Given Cathy is a CaffeinateMe customer
+    When Cathy orders a "large cappuccino" with a comment "Double sugar"
+    Then Barry should receive the order
+    And the order should have the comment "Double sugar"
